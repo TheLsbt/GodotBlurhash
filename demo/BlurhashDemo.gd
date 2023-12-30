37d6777169d695b26fbc70d2e1f83cd0ad51f5e9
TheLsbt <tyresewolmarans@gmail.com>
2023-12-30 22:17:04 +0200

    Update BlurhashDemo.gd

--- demo/BlurhashDemo.gd
+++ demo/BlurhashDemo.gd
@@ -1,7 +1,5 @@
 extends Node
 
-# Our valid blurhash
-#LEHV6nWB2yk8pyo0adR*.7kCMdnj
 @onready var original: TextureRect = $Original
 @onready var output: TextureRect = $Blurhash
 
@@ -9,5 +7,4 @@ extends Node
 func _ready() -> void:
 	var texture := original.texture
 	var blurhash := Blurhash.encode(texture, 4, 3)
-	#print(bh.is_blurhash_valid(blurhash))
-	output.texture = Blurhash.blurhash_decode(blurhash, 100, 60, 1)
+	output.texture = Blurhash.decode(blurhash, 100, 60, 1)
